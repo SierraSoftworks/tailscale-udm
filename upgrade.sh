72616c4092aee1f599cdbeb55d0c337da52f653b
Benjamin Pannell <benjamin@pannell.dev>
2021-05-11 08:37:23 +0100

    feat: Add an upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-VERSION="1.8.0"
+VERSION="${1?Version must be provided as the first argument to this script}"
 
 echo "Installing Tailscale in /mnt/data/tailscale"
 rm -f /tmp/tailscale.tgz
@@ -11,12 +11,13 @@ tar xzf /tmp/tailscale.tgz -C /tmp/tailscale
 mkdir -p /mnt/data/tailscale
 cp -R /tmp/tailscale/tailscale_${VERSION}_arm64/* /mnt/data/tailscale/
 
-echo "Installing boot script for Tailscale"
-curl -o /mnt/data/on_boot.d/10-tailscaled.sh https://raw.github.com/SierraSoftworks/tailscale-udm/master/on_boot.d/10-tailscaled.sh
-chmod +x /mnt/data/on_boot.d/10-tailscaled.sh
+echo "Shutting down tailscaled"
+/mnt/data/tailscale/tailscale down
+killall tailscaled
 
 echo "Starting tailscaled service"
 /mnt/data/on_boot.d/10-tailscaled.sh
+
 sleep 5
 
 echo "Starting tailscale"
