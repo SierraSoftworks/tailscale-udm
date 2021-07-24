64029c33ad90f991fb8e4f1ec909c7ca9d9f182f
Juan Patino <juandp77@gmail.com>
2021-07-24 09:58:31 -0500

    version 1.10.2

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-VERSION="${1?Version must be provided as the first argument to this script}"
+VERSION="1.10.2"
 
 echo "Installing Tailscale in /mnt/data/tailscale"
 rm -f /tmp/tailscale.tgz
