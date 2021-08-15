639d4bf8f08074211949bcfbfd4df69bfaf816b4
Philip K. Warren <pkwarren@gmail.com>
2021-08-15 11:06:41 -0500

    Redirect logs to /dev/null - no log rotation.

--- on_boot.d/10-tailscaled.sh
+++ on_boot.d/10-tailscaled.sh
@@ -6,4 +6,8 @@ FLAGS="--tun userspace-networking"
 ## starts tailscaled
 
 /mnt/data/tailscale/tailscaled --cleanup
-nohup /mnt/data/tailscale/tailscaled --state=/mnt/data/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --port $PORT $FLAGS > /var/log/tailscaled.log 2>&1 &
+nohup /mnt/data/tailscale/tailscaled \
+    --state=/mnt/data/tailscale/tailscaled.state \
+    --socket=/var/run/tailscale/tailscaled.sock \
+    --port $PORT \
+    $FLAGS > /dev/null 2>&1 &
