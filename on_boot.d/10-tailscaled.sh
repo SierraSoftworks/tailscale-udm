0bae75d6164a789ffa768ceed90882df44c0da96
Philip K. Warren <pkwarren@gmail.com>
2021-08-15 10:59:09 -0500

    Small tweaks.

--- on_boot.d/10-tailscaled.sh
+++ on_boot.d/10-tailscaled.sh
@@ -6,4 +6,4 @@ FLAGS="--tun userspace-networking"
 ## starts tailscaled
 
 /mnt/data/tailscale/tailscaled --cleanup
-nohup /mnt/data/tailscale/tailscaled --state=/mnt/data/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --port $PORT $FLAGS > /var/log/tailscaled.log &
+nohup /mnt/data/tailscale/tailscaled --state=/mnt/data/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --port $PORT $FLAGS > /var/log/tailscaled.log 2>&1 &
