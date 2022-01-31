10c81cf093980f92c13c814950f47163721c85e5
maxpayne7978 <98770009+maxpayne7978@users.noreply.github.com>
2022-01-31 19:09:58 +0100

    Update manage.sh

--- package/manage.sh
+++ package/manage.sh
@@ -61,7 +61,7 @@ tailscale_install() {
 
   echo "Installing Tailscale v${VERSION} in ${TAILSCALE_ROOT}..."
   curl -sSL -o "${TAILSCALE_TGZ}" "https://pkgs.tailscale.com/stable/tailscale_${VERSION}_arm64.tgz"
-  tar xzf "${TAILSCALE_TGZ}" -C "${WORKDIR}"
+  tar xfz "${TAILSCALE_TGZ}" -C "${WORKDIR}"
   mkdir -p "${TAILSCALE_ROOT}"
   cp -R "${WORKDIR}/tailscale_${VERSION}_arm64"/* "${TAILSCALE_ROOT}"
   
