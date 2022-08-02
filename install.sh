7fb3d44e9e8e6a1c525b543ba8843abe4f3c62a6
Benjamin Pannell <1760260+notheotherben@users.noreply.github.com>
2022-08-02 09:00:03 +0100

    fix: Use ipv4 flag for bootstrapping script (closes #27)

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 set -e
 
 # Determine the latest version of the Tailscale UDM package
-VERSION="${1:-$(curl -sSLq 'https://api.github.com/repos/SierraSoftworks/tailscale-udm/releases' | jq -r '.[0].tag_name')}"
+VERSION="${1:-$(curl -sSLq --ipv4 'https://api.github.com/repos/SierraSoftworks/tailscale-udm/releases' | jq -r '.[0].tag_name')}"
 
 # Setup a temporary directory to download the package
 WORKDIR="$(mktemp -d || exit 1)"
 trap 'rm -rf ${WORKDIR}' EXIT
 
 # Download the Tailscale-UDM package
-curl -sSLf -o "${WORKDIR}/tailscale.tgz" "https://github.com/SierraSoftworks/tailscale-udm/releases/download/${VERSION}/tailscale-udm.tgz"
+curl -sSLf --ipv4 -o "${WORKDIR}/tailscale.tgz" "https://github.com/SierraSoftworks/tailscale-udm/releases/download/${VERSION}/tailscale-udm.tgz"
 
 # Extract the package
 tar xzf "${WORKDIR}/tailscale.tgz" -C "/mnt/data/"
