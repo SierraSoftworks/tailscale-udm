49b4a62ac163498d04dd4fef9d8bba4f829021a5
Benjamin Pannell <benjamin@pannell.dev>
2023-03-28 23:59:19 +0100

    test: Update test assertions

--- tests/status.2.x.test.sh
+++ tests/status.2.x.test.sh
@@ -15,7 +15,7 @@ export TAILSCALED_SOCK="${WORKDIR}/tailscaled.sock"
 export PATH="${WORKDIR}:${PATH}"
 mock "${WORKDIR}/ubnt-device-info" "2.0.0"
 
-assert_eq "$("${ROOT}/package/manage.sh" status)" "Tailscaled is not installed"
+assert_eq "$("${ROOT}/package/manage.sh" status)" "Tailscaled is not installed" "Tailscaled should be reported as not installed"
 
 mock "${WORKDIR}/tailscale" "0.0.0"
 
